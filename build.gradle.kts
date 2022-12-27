95219b2d6e5417cbb4d3a4a14134e875b8531c32
Daniel Demidko <demidko@farpost.com>
2022-12-27 18:57:14 +0500

    stable kotlin & spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ repositories {
   maven("https://repo.spring.io/snapshot")
 }
 plugins {
-  id("org.springframework.boot") version "3.0.2-SNAPSHOT"
+  id("org.springframework.boot") version "3.0.1"
   id("io.spring.dependency-management") version "1.1.0"
-  kotlin("jvm") version "1.8.0-RC2"
-  kotlin("plugin.spring") version "1.8.0-RC2"
+  kotlin("jvm") version "1.7.22"
+  kotlin("plugin.spring") version "1.7.22"
 }
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
