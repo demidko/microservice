640210e4a5006cd15fd223aaa5460c97106356c3
Daniil Demidko <demidko@farpost.com>
2025-02-02 09:07:16 -0300

    deps updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 plugins {
   id("org.springframework.boot") version "3.4.2-SNAPSHOT"
   id("io.spring.dependency-management") version "1.1.7"
-  kotlin("jvm") version "2.1.0"
-  kotlin("plugin.spring") version "2.1.0"
+  kotlin("jvm") version "2.1.10"
+  kotlin("plugin.spring") version "2.1.10"
 }
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
--- src/main/kotlin/app/example/App.kt
+++ src/main/kotlin/app/example/App.kt
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.Duration
+import java.time.Instant
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors.newVirtualThreadPerTaskExecutor
+import kotlin.time.measureTime
 
 @EnableScheduling
 @SpringBootConfiguration
@@ -20,5 +23,8 @@ class App {
 }
 
 fun main(args: Array<String>) {
+  val a = Instant.now()
+  val b = Instant.now()
+  Duration.between(a, b).toMillis();
   runApplication<App>(*args)
 }
