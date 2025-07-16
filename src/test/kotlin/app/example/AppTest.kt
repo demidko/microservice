25a80198c1c127b13438ad2b8d580bfa21949424
Daniil Demidko <demidko@farpost.com>
2025-07-16 09:44:16 -0300

    -annotation class ManualTest

--- src/test/kotlin/app/example/AppTest.kt
+++ src/test/kotlin/app/example/AppTest.kt
@@ -1,7 +1,7 @@
 package app.example
 
-import app.example.annotations.ManualTest
 import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,7 +19,7 @@ class AppTest {
   }
 
   @Test
-  @ManualTest
+  @Tag("manual")
   fun manualTest() {
     throw RuntimeException()
   }
